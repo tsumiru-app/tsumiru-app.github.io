935d2dcde7cfba8ac606d00da3ce054c07b74fb2
Aaron Bamblett <valyth@gmail.com>
2026-06-18 23:07:54 -0700

    Drop Komikku Safari pinned-tab mask icon

--- src/.vitepress/config/headConfig.ts
+++ src/.vitepress/config/headConfig.ts
@@ -10,7 +10,6 @@ const headConfig: HeadConfig[] = [
   ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
   ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
   ['link', { rel: 'manifest', href: '/site.webmanifest' }],
-  ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#7c7bff' }],
   ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
   ['meta', { name: 'twitter:card', content: 'summary' }],
   ['meta', { property: 'og:site_name', content: 'Tsumiru' }],
